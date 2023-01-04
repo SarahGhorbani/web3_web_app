288c8d370aa96934e18c183ce424c075b9a5c363
SaraGhorbani <sara.qorbani73@gmail.com>
2023-01-04 15:14:17 +0330

    clean code

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -22,9 +22,6 @@ class Home extends StatelessWidget {
               } else if (h.isEnabled){
                 return TextButton(onPressed: h.connect, child: const Text('connect'));
               }
-              // return NikuButton.outlined(
-              //   'Connect'.text.bold().fontSize(20),
-              // ).onPressed(h.connect);
               else {
                 shown = 'Your browser is not supported!';
               }
@@ -33,34 +30,14 @@ class Home extends StatelessWidget {
             }),
             const SizedBox(height: 30),
             if (h.isConnected && h.isInOperatingChain)
-            // [
               TextButton(onPressed: h.getCakeTokenBalance, child: const Text('fetch ur cake!')),
-            // NikuButton('fetch ur cake!'.text.fontSize(20))
-            //     .onPressed(h.getCakeTokenBalance),
             TextButton(onPressed: h.burnSomeCake, child: const Text('burn some cake?! (scary)')),
-            // NikuButton('burn some cake?! (scary)'.text.fontSize(20))
-            //     .onPressed(h.burnSomeCake),
             Text('Your cake balance is ${h.yourCakeBalance} wei'),
-            //   'Your cake balance is ${h.yourCakeBalance} wei'
-            //       .text
-            //       .fontSize(18)
-            // ].wrap.spacing(10).crossCenter(),
             if (h.isConnected && h.isInOperatingChain) ...[
-              // [
               TextButton(onPressed: h.getMasterChefInformation, child: const Text('fetch masterchef information!')),
-              // NikuButton('fetch masterchef information!'.text.fontSize(20))
-              //     .onPressed(h.getMasterChefInformation),
               Text('Current cake per block is ${h.cakePerBlock} wei/block'),
-              // 'Current cake per block is ${h.cakePerBlock} wei/block'
-              //     .text
-              //     .fontSize(18),
               Text('Current pool length is ${h.poolLength}'),
-              // 'Current pool length is ${h.poolLength}'.text.fontSize(18),
-
-              // ].wrap.spacing(10).crossCenter(),
               TextButton(onPressed: h.burnSomeCake, child: const Text('EMERGENCY WITHDRAW AT POOL 0!!!')),
-              // NikuButton('EMERGENCY WITHDRAW AT POOL 0!!!'.text.fontSize(20))
-              //     .onPressed(h.burnSomeCake),
             ]
           ].column,
         ),
